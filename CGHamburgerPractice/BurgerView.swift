dd77978c9ea2529240f467e01a596566e6a1bf91
Jager-yoo <y73447jh@gmail.com>
2022-02-19 20:27:07 +0900

    feat: 토마토 한 조각 그리기

--- CGHamburgerPractice/BurgerView.swift
+++ CGHamburgerPractice/BurgerView.swift
@@ -11,9 +11,9 @@ import UIKit
 final class BurgerView: UIView {
     
     @IBInspectable private var lineWidth: CGFloat = 10
-    @IBInspectable private var strokeColor: UIColor = .brown
+    @IBInspectable private var bunStrokeColor: UIColor = .brown
     @IBInspectable private var lettuceStrokeColor: UIColor = .green
-    @IBInspectable private var fillColor: UIColor = .systemBrown
+    @IBInspectable private var tomatofillColor: UIColor = .red
     @IBInspectable private var cornerRadius: CGFloat = 5
     
     override func draw(_ rect: CGRect) {
@@ -24,13 +24,9 @@ final class BurgerView: UIView {
         let height = bounds.height
         let width = bounds.width
         
+        // 아래 번 그리기
         let bottomBunOrigin = CGPoint(x: width * 0.15, y: height * 0.64)
         let bottomBunSize = CGSize(width: width * 0.7, height: height * 0.05)
-        
-        let meatPattyStartPoint = CGPoint(x: width * 0.12, y: height * 0.6)
-        let meatPattyEndPoint = CGPoint(x: width * 0.88, y: height * 0.6)
-        
-        let lettuceStartPoint = CGPoint(x: width * 0.12, y: height * 0.55)
         
         let bottomBun = UIBezierPath(
             roundedRect: CGRect(
@@ -40,18 +36,25 @@ final class BurgerView: UIView {
             cornerRadius: cornerRadius
         )
         bottomBun.lineWidth = lineWidth
-        strokeColor.setStroke()
+        bunStrokeColor.setStroke()
         bottomBun.stroke()
         bottomBun.close()
+        
+        // 고기 패티 그리기
+        let meatPattyStartPoint = CGPoint(x: width * 0.12, y: height * 0.6)
+        let meatPattyEndPoint = CGPoint(x: width * 0.88, y: height * 0.6)
         
         let meatPatty = UIBezierPath()
         meatPatty.lineWidth = 40
         meatPatty.lineCapStyle = .round
         meatPatty.move(to: meatPattyStartPoint)
         meatPatty.addLine(to: meatPattyEndPoint)
-        strokeColor.setStroke()
+        bunStrokeColor.setStroke()
         meatPatty.stroke()
         meatPatty.close()
+        
+        // 양상추 그리기
+        let lettuceStartPoint = CGPoint(x: width * 0.12, y: height * 0.55)
         
         let lettuce = UIBezierPath()
         lettuce.lineWidth = 20
@@ -70,5 +73,19 @@ final class BurgerView: UIView {
         lettuceStrokeColor.setStroke()
         lettuce.stroke()
         lettuce.close()
+        
+        // 토마토 그리기
+        let tomatoOrigin = CGPoint(x: width * 0.13, y: height * 0.49)
+        let tomatoSize = CGSize(width: width * 0.35, height: height * 0.03)
+        
+        let tomato = UIBezierPath(
+            rect: CGRect(
+                origin: tomatoOrigin,
+                size: tomatoSize
+            )
+        )
+        tomatofillColor.setFill()
+        tomato.fill()
+        tomato.close()
     }
 }
